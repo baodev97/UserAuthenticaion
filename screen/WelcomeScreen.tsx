37ff93dd06ca06094e86fb9da16646abbea6bdeb
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-10 20:31:14 +0700

    get data from BE

--- screen/WelcomeScreen.tsx
+++ screen/WelcomeScreen.tsx
@@ -1,10 +1,26 @@
+import { AuthContext } from '@/store/auth-context';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen() {
+    const [messageFetching, setMessagefetching] = useState<string|null>(null);
+    const AuthCtx = useContext(AuthContext)
+
+    useEffect(()=>{
+       axios.get('https://expensetrackapi-default-rtdb.asia-southeast1.firebasedatabase.app/message.json').then(
+        (res)=>{
+            setMessagefetching(res.data)
+        }
+       )
+       
+    },[])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{messageFetching}</Text>
     </View>
   );
 }
