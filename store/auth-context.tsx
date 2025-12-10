6846d13448d0081c192162c0cca74fc8d162000c
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-10 17:09:55 +0700

    storing & Managing the User Authentication State with context

--- store/auth-context.tsx
+++ store/auth-context.tsx
@@ -1,22 +1,37 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 type AuthContextProviderProps = {
   children: ReactNode;
 };
+type AuthContextinit = {
+  token: string|null
+  isAuthenticated: boolean
+  authenticate: (token: string) => void;
+  logout: () => void;
+};
 
-export const AuthContext = createContext({
+export const AuthContext = createContext<AuthContextinit>({
   token: "",
   isAuthenticated: false,
-  authenticate: () => {},
+  authenticate: (token: string | null) => {},
   logout: () => {},
 });
 
 function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [authToken, setAuthToken] = useState<string | null>(null);
+
+  function authenticate(token: string) {
+    setAuthToken(token);
+  }
+  function logout() {
+    setAuthToken(null);
+  }
+
   const value = {
-    token: "",
-    isAuthenticated: false,
-    authenticate: () => {},
-    logout: () => {},
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
